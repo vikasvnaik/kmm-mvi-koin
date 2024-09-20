3c19e1406a7f21cce45f61e1440211a1a67342b0
vikasvnaik <vikasvs42@gmail.com>
2024-09-20 18:35:58 +0530

    Setup to redirect from Android activity and IOS ContentView to Compose

--- androidApp/src/main/java/com/android/kmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/android/kmm/android/MainActivity.kt
@@ -3,38 +3,21 @@ package com.android.kmm.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.android.kmm.Greeting
+import com.android.kmm.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    GreetingView(Greeting().greet())
-                }
-            }
+            App()
         }
     }
 }
 
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
 @Preview
 @Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
-    }
+fun AppAndroidPreview() {
+    App()
 }
