8df86fe08ecfae3626bb84716888173b0e081337
vikasvnaik <vikasvs42@gmail.com>
2024-09-28 13:09:38 +0530

    LazyColumn to show the post list

--- shared/src/commonMain/kotlin/com/android/kmm/App.kt
+++ shared/src/commonMain/kotlin/com/android/kmm/App.kt
@@ -1,14 +1,19 @@
 package com.android.kmm
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.android.kmm.presentation.home.HomeUiState
 import com.android.kmm.presentation.home.HomeViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -22,7 +27,46 @@ fun App(homeViewModel: HomeViewModel = koinInject()) {
 }
 
 @Composable
-fun appView(state: HomeUiState){
-    state.posts
-    println("Posts data : " +state.posts)
+fun appView(state: HomeUiState) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Posts")
+                }
+            )
+        }
+    ) { padding ->
+        LazyColumn(modifier = Modifier.padding(padding)) {
+            items(state.posts, key = {it.id}) { post ->
+                PostView(post.title, post.id, post.body)
+            }
+        }
+
+    }
 }
+
+@Composable
+private fun PostView(title: String, id: Long, body: String) {
+    Column {
+        Text(
+            text = title,
+            style = typography.h5,
+            modifier = Modifier
+                .padding(start = 24.dp, end = 24.dp, top = 16.dp, bottom = 4.dp)
+        )
+        Text(
+            text = id.toString(),
+            style = typography.caption,
+            modifier = Modifier.padding(horizontal = 24.dp, vertical = 4.dp)
+        )
+        Text(
+            text = body,
+            style = typography.body1,
+            modifier = Modifier
+                .padding(start = 24.dp, end = 24.dp, top = 4.dp, bottom = 16.dp)
+        )
+        Divider()
+    }
+}
+
